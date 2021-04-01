2b556d32ba0537e7545f5edb96e049a838928ac3
Braden <rupp.braden@principal.com>
2021-04-01 18:53:54 +0000

    deleting stuff

--- src/repository/statusRepository.ts
+++
@@ -1,9 +0,0 @@
-
-export enum Status {
-    Verified,
-    Unverified
-}
-
-export const getStatusByFullName = (_name: string): Status => {
-    throw new Error('Database Connection needed')
-};
--- src/service/accountStatusService.ts
+++
@@ -1,7 +0,0 @@
-import { getStatusByFullName, Status } from "../repository/statusRepository"
-
-export const getStatus = (firstName: string, lastName: string): Status => {
-
-    const fullName = `${firstName.toUpperCase()}_${lastName.toUpperCase()}`
-    return getStatusByFullName(fullName);
-}
